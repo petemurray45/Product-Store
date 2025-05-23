b53606353b74b1a33c296df3282bb67bbfc25f2c
petemurray45 <pmurray697@gmail.com>
2025-05-24 00:27:38 +0100

    safety commit

--- frontend/product-store/src/App.jsx
+++ frontend/product-store/src/App.jsx
@@ -1,7 +1,20 @@
+import NavBar from "./components/NavBar";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
+
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl text-red-500">Hey</h1>
+    <div
+      className="min-h-screen bg-base-200 transition-colors duration-300"
+      data-theme="forest"
+    >
+      <NavBar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product/:id" element={<ProductPage />} />
+      </Routes>
     </div>
   );
 }
