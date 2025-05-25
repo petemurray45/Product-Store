ad0bcc4dac8b95cc3ab4b029bf7cbcd60227e699
petemurray45 <pmurray697@gmail.com>
2025-05-25 14:44:22 +0100

    added product cards to homepage with styling

---
+++ frontend/product-store/src/components/ProductCard.jsx
@@ -0,0 +1,42 @@
+import { Edit, TrashIcon } from "lucide-react";
+import React from "react";
+import { Link } from "react-router-dom";
+
+function ProductCard({ product }) {
+  return (
+    <div className="card bg-base-100 shadow-xl hover:shadow-xl transition-shadow duration-300">
+      {/*Product Image*/}
+      <figure className="relative pt-[56.25%]">
+        <img
+          src={product.image}
+          alt={product.name}
+          className="absolute top-0 left-0 w-full h-full object-cover"
+        />
+      </figure>
+
+      <div className="card-body">
+        {/*Product Info*/}
+        <h2 className="card-title text-lg font-semibold">{product.name}</h2>
+        <p className="text-2xl font-bold text-primary">
+          ${Number(product.price).toFixed(2)}
+        </p>
+
+        {/*Card Actions*/}
+        <div className="card-actions justify-end mt-4">
+          <Link
+            to={`/product/${product.id}`}
+            className="btn btn-sm btn-info btn-outline"
+          >
+            <Edit className="size-4" />
+          </Link>
+
+          <button className="btn btn-sm btn-error btn-outline">
+            <TrashIcon className="size-4" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductCard;
